541a91603d6a9df19b379f2465c118738b2c836b
bisrat yared <bisremv@gmail.com>
2023-04-10 17:13:06 +0300

    final project may add some features in the future

--- script.js
+++ script.js
@@ -3,50 +3,75 @@ for (let i = 0; i < document.querySelectorAll(".btn").length; i++) {
     let char = this.innerHTML;
     enterNumOpr(char);
     displayAns();
-    let save = char;
   });
 }
 function displayAns() {
   let str = document.querySelector(".caculate").innerHTML;
   let result = eval(str);
   document.querySelector(".dis").innerHTML = result;
+ 
 }
 function enterNumOpr(num) {
   if (
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(parseInt(num)) ||
     ["-", "/", "+", "*"].includes(num)
   ) {
-    if (document.querySelector(".caculate").textContent == null) {
-      document.querySelector(".dis").innerHTML = " ";
-    }
     if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(parseInt(num))) {
       document.querySelector(".caculate").textContent += num;
     }
 
     if (
       document.querySelector(".caculate").textContent != null &&
-      ["-", "/", "+", "*"].includes(num)
+      ["-", "/", "+", "*"].includes(String(num))
     ) {
       document.querySelector(".caculate").textContent += num;
     }
   }
-   
 }
 document.querySelector(".b").addEventListener("click", function () {
-  let var1 = document.querySelector(".caculate").innerHTML;
-   document.querySelector(".caculate").innerHTML= var1.slice(0,-1);
-   let result = eval(var1.slice(0,-1));
-  document.querySelector(".dis").innerHTML = result;
-  });
+  if (document.querySelector(".caculate").textContent.length == 1||  document.querySelector(".caculate").textContent.length == 0 || document.querySelector(".dis").textContent.length == 1) {
+    document.querySelector(".caculate").innerHTML = null;
+    document.querySelector(".dis").textContent = null;
+ 
+  }
+  else {
+    let var1 = document.querySelector(".caculate").innerHTML;
+    document.querySelector(".caculate").innerHTML = var1.slice(0, -1);
+    let result = eval(var1.slice(0, -1));
+    document.querySelector(".dis").innerHTML = result;
+  }
+});
 document.querySelector(".c").addEventListener("click", function () {
-  document.querySelector(".caculate").innerHTML=null;
+  if (document.querySelector(".dis").innerHTML != null) {
+    document.querySelector(".caculate").innerHTML = null;
     document.querySelector(".dis").innerHTML = null;
-  
+  }
 });
 document.querySelector("#eql").addEventListener("click", function () {
-  let save = document.querySelector(".caculate").innerHTML+"="+document.querySelector(".dis").innerHTML;
-  document.querySelector(".his").innerHTML +=" <P> "+save+"</P>";
-  document.querySelector(".caculate").innerHTML=null;
-  document.querySelector(".dis").innerHTML = null;
+  let str = document.querySelector(".caculate").textContent;
+let detect=0;
+  for(let i=0; i<str.length;i++){
+    if(["-", "/", "+", "*"].includes(str[i])){
+      if(["-", "/", "+", "*"].includes(str[i+1])){
+        if(!("*"==str[i+1]&&"*"==str[i])){
+              detect++;
+      }}
+  }
+}
+ 
   
+  let save =
+    document.querySelector(".caculate").innerHTML +
+    "=" +
+    document.querySelector(".dis").innerHTML;
+  if (detect==0){
+    document.querySelector(".his").innerHTML += " <P> " + save + "</P>";
+    
+  } 
+  else if (detect=1)
+  {
+    document.querySelector(".caculate").textContent = "Error the input is incorect";
+ 
+    document.querySelector(".dis").textContent = "Error the input is incorect"; 
+  }
 });
