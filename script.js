ff9611491730c13a9591d5b38aeb56d74d53315f
bisrat yared <bisremv@gmail.com>
2023-04-10 16:13:16 +0300

    only thig left is error & exception handling

--- script.js
+++ script.js
@@ -1,73 +1,52 @@
 for (let i = 0; i < document.querySelectorAll(".btn").length; i++) {
   document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     let char = this.innerHTML;
-    enterNum(char);
+    enterNumOpr(char);
     displayAns();
+    let save = char;
   });
 }
 function displayAns() {
   let str = document.querySelector(".caculate").innerHTML;
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
+  let result = eval(str);
+  document.querySelector(".dis").innerHTML = result;
+}
+function enterNumOpr(num) {
+  if (
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(parseInt(num)) ||
+    ["-", "/", "+", "*"].includes(num)
+  ) {
+    if (document.querySelector(".caculate").textContent == null) {
+      document.querySelector(".dis").innerHTML = " ";
+    }
+    if ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0].includes(parseInt(num))) {
+      document.querySelector(".caculate").textContent += num;
+    }
+
     if (
-      str[i] == "/" ||
-      str[i] == "*" ||
-      str[i] == "-" ||
-      str[i] == "+" ||
-      str[i] == "="
+      document.querySelector(".caculate").textContent != null &&
+      ["-", "/", "+", "*"].includes(num)
     ) {
-      count++;
+      document.querySelector(".caculate").textContent += num;
     }
   }
-  if (count < 1) {
-    document.querySelector(".dis").innerHTML = str;
-  } 
-  else if (count > 0) {
-  }
-
+   
 }
-function checkForbr(str){
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] == "(") {
-          while (i < str.length) {
-            
-          }
-        }
-      }
-
+document.querySelector(".b").addEventListener("click", function () {
+  let var1 = document.querySelector(".caculate").innerHTML;
+   document.querySelector(".caculate").innerHTML= var1.slice(0,-1);
+   let result = eval(var1.slice(0,-1));
+  document.querySelector(".dis").innerHTML = result;
+  });
+document.querySelector(".c").addEventListener("click", function () {
+  document.querySelector(".caculate").innerHTML=null;
+    document.querySelector(".dis").innerHTML = null;
   
-}
-function checkFormul(str){
-    for (let i = 0; i < str.length; i++) {
-        let lastidx =0;
-        
-        if (
-            str[i] == "-" ||
-            str[i] == "+" ) {
-                lastidx =i;
-         
-        }
-        if (str[i] == "*") {
-           
-        }
-      }
+});
+document.querySelector("#eql").addEventListener("click", function () {
+  let save = document.querySelector(".caculate").innerHTML+"="+document.querySelector(".dis").innerHTML;
+  document.querySelector(".his").innerHTML +=" <P> "+save+"</P>";
+  document.querySelector(".caculate").innerHTML=null;
+  document.querySelector(".dis").innerHTML = null;
   
-}
-function add(a, b) {
-  return a + b;
-}
-function sub(a, b) {
-  return a - b;
-}
-function mul(a, b) {
-  return a * b;
-}
-function div(a, b) {
-  return a / b;
-}
-function enterNum(num) {
-  if (document.querySelector(".caculate").textContent == "") {
-    document.querySelector(".dis").innerHTML = " ";
-  }
-  document.querySelector(".caculate").textContent += num;
-}
+});
